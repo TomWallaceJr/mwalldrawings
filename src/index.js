113cbc8f628c3be7cf550f7a3165c9806f3c84b0
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-04-02 18:15:48 -0400

    creates custom css styles and display generic header component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App/App'
-import { BrowserRouter } from 'react-router-dom'
+
 
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
   ,
   document.getElementById('root')
 );
