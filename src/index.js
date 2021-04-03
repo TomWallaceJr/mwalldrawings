b9e9160e7545d1e672437e391f89046b9335ad5e
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-04-03 12:56:52 -0400

    react bootstrap

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App/App'
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-  ,
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
-
