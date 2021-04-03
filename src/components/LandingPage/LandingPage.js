377952fb7ae7a33737763126ce8356a9e07b0da5
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-04-03 14:31:24 -0400

    implements photo carousel on landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Slideshow from '../Slideshow/Slideshow';
+import CarouselContainer from '../Carousel/CarouselContainer';
 
 export default function LandingPage() {
   return (
     <main>
-      <Slideshow />
+      <CarouselContainer />
     </main>
   );
 }
