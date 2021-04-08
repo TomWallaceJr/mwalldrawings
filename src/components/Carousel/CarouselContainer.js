0db0d395825775ce0edcd5277ce73af52131e120
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-04-07 23:54:21 -0400

    redesigns carousel

--- src/components/Carousel/CarouselContainer.js
+++ src/components/Carousel/CarouselContainer.js
@@ -3,28 +3,29 @@ import { Carousel } from 'react-bootstrap';
 import image1 from '../../images/eagle.jpg';
 import image2 from '../../images/lovebirds.jpg';
 import image3 from '../../images/storm.jpg';
+import './Carousel.css';
 
 export default function CarouselContainer() {
   return (
-    <Carousel fade>
+    <Carousel fade className="carousel">
       <Carousel.Item>
-        <img className="d-block w-100" src={image1} alt="First slide" />
+        <img className="carousel-img" src={image1} alt="First slide" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={image2} alt="Second slide" />
 
+      <Carousel.Item>
+        <img className="carousel-img" src={image2} alt="Second slide" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={image3} alt="Third slide" />
 
+      <Carousel.Item>
+        <img className="carousel-img" src={image3} alt="Third slide" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
