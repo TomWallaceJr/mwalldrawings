a0bda616fef7eb5e1e29ce8765a01ce630b63232
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-04-08 02:06:48 -0400

    sets up helper file that will hold all of melissas project details

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
   return (
     <header className="header-container">
-      <h1 className="header-title pt-3">MWall Drawings</h1>
+      <Link to="/">
+        <h1 className="header-title pt-3">MWall Drawings</h1>
+      </Link>
     </header>
   );
 }
---
+++ src/helpers/portfolio.js
@@ -0,0 +1,19 @@
+// This is an array of objects where each object is a project done by melissa with
+// properties associated with them { title img description type style }
+
+export const portfolio = [
+  {
+    title: 'owl',
+    img: 'imgurl',
+    description: 'sdfghd sdfg',
+    type: 'misc',
+    style: 'acrylic',
+  },
+  {
+    title: 'eagle',
+    img: 'imgurl',
+    description: 'sdfghd sdfg',
+    type: 'pet',
+    style: 'paint',
+  },
+];
