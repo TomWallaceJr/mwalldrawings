8663721a08b08fcf9e532dea0aa72892c4816a91
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-04-12 23:32:26 -0400

    beginning of portfolio import in helper file

--- src/helpers/portfolio.js
+++ src/helpers/portfolio.js
@@ -1,6 +1,7 @@
 // This is an array of objects where each object is a project done by melissa with
 // properties associated with them { title img description type style }
 // When populating pages with appropriate artwork we will pull from this file
+import family from '../images/family.jpg';
 
 export const portfolio = [
   {
@@ -16,5 +17,12 @@ export const portfolio = [
     description: 'sdfghd sdfg',
     type: 'pet',
     style: 'paint',
+  },
+  {
+    title: 'family',
+    img: { family },
+    description: 'A portrait of my son and two nieces.',
+    type: 'people',
+    style: 'pastel',
   },
 ];
