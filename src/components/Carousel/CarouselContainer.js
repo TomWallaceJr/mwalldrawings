90ad4341b2f074ad4aed777a3a421124a153cfd4
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-04-13 23:16:12 -0400

    changed carousel images

--- src/components/Carousel/CarouselContainer.js
+++ src/components/Carousel/CarouselContainer.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import image1 from '../../images/eagle.jpg';
-import image2 from '../../images/lovebirds.jpg';
-import image3 from '../../images/storm.jpg';
+import koval from '../../images/koval.jpg';
+import grandkids from '../../images/grandkids.jpg';
+import ally from '../../images/ally.jpg';
 import './Carousel.css';
 
 export default function CarouselContainer() {
   return (
     <Carousel fade className="carousel">
       <Carousel.Item>
-        <img className="carousel-img" src={image1} alt="First slide" />
+        <img className="carousel-img" src={koval} alt="First slide" />
         <Carousel.Caption>
           <h3 className="label">Pet Portraits</h3>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
-        <img className="carousel-img" src={image2} alt="Second slide" />
+        <img className="carousel-img" src={grandkids} alt="Second slide" />
         <Carousel.Caption>
           <h3 className="label">Portaits</h3>
         </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
-        <img className="carousel-img" src={image3} alt="Third slide" />
+        <img className="carousel-img" src={ally} alt="Third slide" />
         <Carousel.Caption>
           <h3 className="label">Acrylics</h3>
         </Carousel.Caption>
