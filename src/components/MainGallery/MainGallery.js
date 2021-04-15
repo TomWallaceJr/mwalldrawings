30def13688fe9dcdeebedd5b142dd1af6c3054f3
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-04-15 17:28:30 -0400

    portfilio img paths corrected

--- src/components/MainGallery/MainGallery.js
+++ src/components/MainGallery/MainGallery.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import portfolio from '../../helpers/portfolio';
 
 export default function MainGallery() {
   return (
--- src/helpers/portfolio.js
+++ src/helpers/portfolio.js
@@ -1,27 +1,26 @@
 // This is an array of objects where each object is a project done by melissa with
 // properties associated with them { title img description type style }
 // When populating pages with appropriate artwork we will pull from this file
-import family from '../images/family.jpg';
 
 export const portfolio = [
   {
-    title: 'owl',
-    img: 'imgurl',
-    description: 'sdfghd sdfg',
+    title: 'Storm',
+    img: '../../images/storm.jpg',
+    description: 'An owl done with ',
     type: 'misc',
     style: 'acrylic',
   },
   {
     title: 'eagle',
-    img: 'imgurl',
-    description: 'sdfghd sdfg',
+    img: '../../images/eagle.jpg',
+    description: 'A bald eagle in the wild',
     type: 'pet',
     style: 'paint',
   },
   {
     title: 'family',
-    img: { family },
-    description: 'A portrait of my son and two nieces.',
+    img: '../../images/family.jpg',
+    description: 'A portrait of my son and nieces.',
     type: 'people',
     style: 'pastel',
   },
