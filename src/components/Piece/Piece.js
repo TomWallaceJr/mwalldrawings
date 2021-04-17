6fd941f51b2300d49bc4204c2082935ff0c9b5f7
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-04-17 12:41:23 -0400

    styling and sizing of images in gallery

--- src/components/Piece/Piece.js
+++ src/components/Piece/Piece.js
@@ -3,13 +3,12 @@ import React from 'react';
 export default class Piece extends React.Component {
   render() {
     // deconstruct properties for Piece
-    const { title, imgPath, description } = this.props.details;
+    const { title, imgPath, description, type } = this.props.details;
 
     return (
-      <div className="single-piece">
-        {console.log(imgPath)}
-        <h1>{title}</h1>
-        <img src={imgPath} alt={title} />
+      <div className="w-100 d-flex flex-column p-2">
+        <h1 className="mx-auto">{title}</h1>
+        <img src={imgPath} alt={title} className="w-100 h-75 mx-auto" />
         <p>{description}</p>
       </div>
     );
