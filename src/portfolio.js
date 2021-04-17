f2f12ae0fb2a41ab6b2dac4cec2abda3b93287c8
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-04-17 12:11:44 -0400

    all pieces imported into portfolio.js array

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,28 +1,146 @@
 // This is an array of objects where each object is a project done by melissa with
-// properties associated with them { title img description type style }
+// properties associated with them { title img description type }
 // When populating pages with appropriate artwork we will pull from this file
+
+// EX:
+// {
+//   title: 'Storm',
+//   imgPath: '/images/storm.jpg',
+//   description: '8in x 8in acrylic painting on stretched canvas  ',
+//   type: 'misc',   /// type either people | pets | misc
+// },
 
 const portfolio = [
   {
     title: 'Storm',
     imgPath: '/images/storm.jpg',
-    description: 'An owl done with ',
+    description: '12" x 12" acrylic painting on stretched canvas.',
     type: 'misc',
-    style: 'acrylic',
   },
   {
-    title: 'eagle',
+    title: 'Noble',
     imgPath: '/images/eagle.jpg',
-    description: 'A bald eagle in the wild',
-    type: 'pet',
-    style: 'paint',
+    description: '12" x 8" acrylic painting on stretched canvas.',
+    type: 'misc',
   },
   {
-    title: 'family',
+    title: 'Grandkids',
     imgPath: '/images/grandkids.jpg',
-    description: 'A portrait of my son and nieces.',
+    description: '11" x 14" pastel drawing on paper.',
     type: 'people',
-    style: 'pastel',
+  },
+  {
+    title: 'Gentle',
+    imgPath: '/images/sheep.jpg',
+    description: '8" x 6" acrylic painting on stretched canvas.',
+    type: 'misc',
+  },
+  {
+    title: 'Soft',
+    imgPath: '/images/rabbit.jpg',
+    description: '8" x 6" acrylic painting on stretched canvas.',
+    type: 'misc',
+  },
+  {
+    title: 'Mother and son',
+    imgPath: '/images/owls.jpg',
+    description: '12" x 12" acrylic painting on stretched canvas.',
+    type: 'misc',
+  },
+  {
+    title: 'Winter Life',
+    imgPath: '/images/winter.jpg',
+    description: '8" x 6" acrylic painting on stretched canvas.',
+    type: 'misc',
+  },
+  {
+    title: 'Baby',
+    imgPath: '/images/baby.jpg',
+    description: '8" x 8" acrylic painting on stretched canvas.',
+    type: 'misc',
+  },
+  {
+    title: 'Serenity',
+    imgPath: '/images/serenity.jpg',
+    description: '10" x 8" acrylic painting on stretched canvas.',
+    type: 'misc',
+  },
+  {
+    title: 'Brielle',
+    imgPath: '/images/brielle.jpg',
+    description: '6" x 8" matted pastel drawing on paper.',
+    type: 'people',
+  },
+  {
+    title: 'Ethan',
+    imgPath: '/images/ethan.jpg',
+    description: '8" x 6" matted pastel drawing on paper.',
+    type: 'people',
+  },
+  {
+    title: 'Jackson',
+    imgPath: '/images/jackson.jpg',
+    description: '8" x 6" matted pastel drawing on paper.',
+    type: 'people',
+  },
+  {
+    title: 'Marcy',
+    imgPath: '/images/marcy.jpg',
+    description: '8" x 6" matted pastel drawing on paper.',
+    type: 'people',
+  },
+  {
+    title: 'Beach Day',
+    imgPath: '/images/beachday.jpg',
+    description: '8" x 6" matted pastel drawing on paper.',
+    type: 'people',
+  },
+  {
+    title: 'Tippi',
+    imgPath: '/images/tippi.jpg',
+    description: '8" x 8" acrylic painting on stretched canvas.',
+    type: 'pets',
+  },
+  {
+    title: 'Abbot1',
+    imgPath: '/images/abbot1.jpg',
+    description:
+      '8" x 8" acrylic painting on stretched canvas with simple background.',
+    type: 'pets',
+  },
+  {
+    title: 'Abbot2',
+    imgPath: '/images/abbot2.jpg',
+    description:
+      '8" x 8" acrylic painting on stretched canvas with simple background.',
+    type: 'pets',
+  },
+  {
+    title: 'Koval',
+    imgPath: '/images/koval.jpg',
+    description:
+      '8" x 8" acrylic painting on stretched canvas with detailed background.',
+    type: 'pets',
+  },
+  {
+    title: 'Lucy',
+    imgPath: '/images/lucy.jpg',
+    description:
+      '10" x 8" acrylic painting on stretched canvas with detailed background.',
+    type: 'pets',
+  },
+  {
+    title: 'Ally',
+    imgPath: '/images/ally.jpg',
+    description:
+      '12" x 12" acrylic painting on stretched canvas with simple background. Model shown',
+    type: 'pets',
+  },
+  {
+    title: 'Lovebirds',
+    imgPath: '/images/lovebirds.jpg',
+    description: '10" x 8" acrylic painting on stretched canvas.',
+    type: 'pets',
   },
 ];
 
