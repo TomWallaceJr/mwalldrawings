2663754cf8058f1942925a6aeea9ba5c3914c117
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-04-24 05:16:02 -0400

    contact route created

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default function ContactMe() {
   return (
     <div>
-      <h1>Contact</h1>
+      <h1 className="text-center">Contact</h1>
+      <p>
+        This page will simple be a form for users to email Melissa... social
+        media links will be provided in the foot er componet not yet created.
+      </p>
     </div>
   );
 }
